fd290b86e214e2d69ca487286965ad73533cc1f1
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-06-11 16:34:47 -0400

    initial code for analyzing reprepresentation of words vs probe

---
+++ extract_condition_response.m
@@ -0,0 +1,13 @@
+function data_out=extract_condition_response(data,info,cond,stim)
+trial_type=info.word_type;
+cond_id=find(cellfun(@(x) x==cond,trial_type));
+data_cond=data(cond_id);
+stim_loc=cellfun(@(x) find(strcmp(x.stimuli_type,stim)), data_cond,'uni',false);
+hilb_zs_ave_cell=cellfun(@(x) x.signal_ave_hilbert_zs_downsample_parsed, data_cond,'uni',false);
+stim_zs_ave_cell=arrayfun(@(x) hilb_zs_ave_cell{x}(stim_loc{x}),[1:size(hilb_zs_ave_cell,1)],'uni',false );
+% reshape stim to correct form if it is not
+stim_zs_ave_cell=cellfun(@(x) cell2mat(reshape(x,1,[])),stim_zs_ave_cell,'uni',false );
+% tensor format (elec*stim(words)*trial)
+stim_zs_ave_tensor=double(cell2mat(permute(stim_zs_ave_cell,[1,3,2])));
+data_out=stim_zs_ave_tensor;
+end
--- struct_rep_analyze_distributed_oscillatory_power.m
+++ struct_rep_analyze_distributed_oscillatory_power.m
@@ -2,12 +2,14 @@ clear all
 close all
 home 
 %% specify where the data is
-data_path='C:\Users\kirsi\Documents\Git\UROP\struct_rep\data'; %change depending on user
-analysis_path=strcat(data_path,'analysis\distributed_oscilatory_power\');
+%data_path='C:\Users\kirsi\Documents\Git\UROP\struct_rep\data'; %change depending on user
+data_path='~/MyData/struct_rep/crunched/';
+%analysis_path=strcat(data_path,'analysis\distributed_oscilatory_power\');
+analysis_path=strcat(data_path,'analysis/distributed_oscilatory_power/');
 subject_id='AMC026';
-d_data= dir(strcat(data_path,'\',subject_id,'*_crunched_v2.mat'));
+d_data= dir(strcat(data_path,'/',subject_id,'*_crunched_v2.mat'));
 fprintf(' %d .mat files were found \n', length(d_data));
-d_data=arrayfun(@(x) strcat(d_data(x).folder,'\',d_data(x).name),[1:length(d_data)]','uni',false);
+d_data=arrayfun(@(x) strcat(d_data(x).folder,'/',d_data(x).name),[1:length(d_data)]','uni',false);
 %% combine responses from all sessions for a given condition. (S= sentence,....)
 sent_word_hilb_ave_tensor_all=[];
 wlist_word_hilb_ave_tensor_all=[];
@@ -19,68 +21,53 @@ for k=1:length(d_data)
     subj=subj.(subj_id{1});
     data=subj.data;
     info=subj.info;
-    
     % sentences 
+
     cond='S';
-    trial_type=info.word_type;
-    cond_id=find(cellfun(@(x) x==cond,trial_type));
-    data_cond=data(cond_id );
-    
-    % sentence, word condition 
     stim='word';
-    stim_loc=cellfun(@(x) find(strcmp(x.stimuli_type,stim)), data_cond,'uni',false);
-    hilb_zs_ave_cell=cellfun(@(x) x.signal_ave_hilbert_zs_downsample_parsed, data_cond,'uni',false);
-    stim_zs_ave_cell=arrayfun(@(x) hilb_zs_ave_cell{x}(stim_loc{x}),[1:size(hilb_zs_ave_cell,1)],'uni',false );
-    % reshape stim to correct form if it is not 
-    stim_zs_ave_cell=cellfun(@(x) cell2mat(reshape(x,1,[])),stim_zs_ave_cell,'uni',false );
-    % tensor format (elec*stim(words)*trial)
-    stim_zs_ave_tensor=double(cell2mat(permute(stim_zs_ave_cell,[1,3,2])));
-    % combine trials for the condition
-    sent_word_hilb_ave_tensor_all=cat(3,sent_word_hilb_ave_tensor_all,stim_zs_ave_tensor);
+    data_out=extract_condition_response(data,info,cond,stim);
+    sent_word_hilb_ave_tensor_all=cat(3,sent_word_hilb_ave_tensor_all,data_out);
     fprintf('added %s\n', strcat(d_data{k},'sent word'))
-    
     % sentence, probe condition 
+    cond='S';
     stim='probe';
-    stim_loc=cellfun(@(x) find(strcmp(x.stimuli_type,stim)), data_cond,'uni',false);
-    hilb_zs_ave_cell=cellfun(@(x) x.signal_ave_hilbert_zs_downsample_parsed, data_cond,'uni',false);
-    stim_zs_ave_cell=arrayfun(@(x) hilb_zs_ave_cell{x}(stim_loc{x}),[1:size(hilb_zs_ave_cell,1)],'uni',false );
-    % reshape stim to correct form if it is not 
-    stim_zs_ave_cell=cellfun(@(x) cell2mat(reshape(x,1,[])),stim_zs_ave_cell,'uni',false );
-    % tensor format (elec*stim(words)*trial)
-    stim_zs_ave_tensor=double(cell2mat(permute(stim_zs_ave_cell,[1,3,2])));
-    % combine trials for the condition
-    sent_probe_hilb_ave_tensor_all=cat(3,sent_probe_hilb_ave_tensor_all,stim_zs_ave_tensor);
+    data_out=extract_condition_response(data,info,cond,stim);
+    sent_probe_hilb_ave_tensor_all=cat(3,sent_probe_hilb_ave_tensor_all,data_out);
     fprintf('added %s\n', strcat(d_data{k},'sent probe'))
-    
     %wordlists
     cond='W';
-    trial_type=info.word_type;
-    cond_id=find(cellfun(@(x) x==cond,trial_type));
-    data_cond=data(cond_id );
-    
-    %wordlist, word condition
     stim='word';
-    stim_loc=cellfun(@(x) find(strcmp(x.stimuli_type,stim)), data_cond,'uni',false);
-    hilb_zs_ave_cell=cellfun(@(x) x.signal_ave_hilbert_zs_downsample_parsed, data_cond,'uni',false);
-    stim_zs_ave_cell=arrayfun(@(x) hilb_zs_ave_cell{x}(stim_loc{x}),[1:size(hilb_zs_ave_cell,1)],'uni',false );
-    % reshape stim to correct form if it is not 
-    stim_zs_ave_cell=cellfun(@(x) cell2mat(reshape(x,1,[])),stim_zs_ave_cell,'uni',false );
-    % tensor format (elec*stim(words)*trial)
-    stim_zs_ave_tensor=double(cell2mat(permute(stim_zs_ave_cell,[1,3,2])));
-    % combine trials for the condition
-    wlist_word_hilb_ave_tensor_all=cat(3,wlist_word_hilb_ave_tensor_all,stim_zs_ave_tensor);
+    data_out=extract_condition_response(data,info,cond,stim);
+    wlist_word_hilb_ave_tensor_all=cat(3,wlist_word_hilb_ave_tensor_all,data_out);
     fprintf('added %s\n', strcat(d_data{k},'wlist word'))
-    
     % wordlist, probe condition 
+    cond='W';
     stim='probe';
-    stim_loc=cellfun(@(x) find(strcmp(x.stimuli_type,stim)), data_cond,'uni',false);
-    hilb_zs_ave_cell=cellfun(@(x) x.signal_ave_hilbert_zs_downsample_parsed, data_cond,'uni',false);
-    stim_zs_ave_cell=arrayfun(@(x) hilb_zs_ave_cell{x}(stim_loc{x}),[1:size(hilb_zs_ave_cell,1)],'uni',false );
-    % reshape stim to correct form if it is not 
-    stim_zs_ave_cell=cellfun(@(x) cell2mat(reshape(x,1,[])),stim_zs_ave_cell,'uni',false );
-    % tensor format (elec*stim(words)*trial)
-    stim_zs_ave_tensor=double(cell2mat(permute(stim_zs_ave_cell,[1,3,2])));
-    % combine trials for the condition
-    wlist_probe_hilb_ave_tensor_all=cat(3,wlist_probe_hilb_ave_tensor_all,stim_zs_ave_tensor);
+    data_out=extract_condition_response(data,info,cond,stim);
+    wlist_probe_hilb_ave_tensor_all=cat(3,wlist_probe_hilb_ave_tensor_all,data_out);
     fprintf('added %s\n', strcat(d_data{k},'wlist probe'))
 end 
+%% compute a cosine distance between word representation over all electrodes during sentence and during probe
+% sentence condition 
+sent_probe_norm=arrayfun(@(x)  sqrt(transpose(sent_probe_hilb_ave_tensor_all(:,:,x))*sent_probe_hilb_ave_tensor_all(:,:,x)),[1:size(sent_probe_hilb_ave_tensor_all,3)],'uni',false);
+sent_words_norm=arrayfun(@(x)  diag(sqrt(transpose(sent_word_hilb_ave_tensor_all(:,:,x))*sent_word_hilb_ave_tensor_all(:,:,x))),[1:size(sent_probe_hilb_ave_tensor_all,3)],'uni',false);
+sent_norms=cellfun(@(x,y) transpose(x.*y) ,sent_probe_norm,sent_words_norm,'uni',false);
+sent_w_vs_probe_dot=arrayfun(@(x)  transpose(sent_probe_hilb_ave_tensor_all(:,:,x))*sent_word_hilb_ave_tensor_all(:,:,x),[1:size(sent_word_hilb_ave_tensor_all,3)],'uni',false);
+sent_w_vs_probe_angle=cellfun(@(x,y) x./y,sent_w_vs_probe_dot,sent_norms,'uni',false);
+
+% wordlist condition 
+wlist_probe_norm=arrayfun(@(x)  sqrt(transpose(wlist_probe_hilb_ave_tensor_all(:,:,x))*wlist_probe_hilb_ave_tensor_all(:,:,x)),[1:size(wlist_probe_hilb_ave_tensor_all,3)],'uni',false);
+wlist_words_norm=arrayfun(@(x)  diag(sqrt(transpose(wlist_word_hilb_ave_tensor_all(:,:,x))*wlist_word_hilb_ave_tensor_all(:,:,x))),[1:size(wlist_probe_hilb_ave_tensor_all,3)],'uni',false);
+wlist_norms=cellfun(@(x,y) transpose(x.*y) ,wlist_probe_norm,wlist_words_norm,'uni',false);
+wlist_w_vs_probe_dot=arrayfun(@(x)  transpose(wlist_probe_hilb_ave_tensor_all(:,:,x))*wlist_word_hilb_ave_tensor_all(:,:,x),[1:size(wlist_word_hilb_ave_tensor_all,3)],'uni',false);
+wlist_w_vs_probe_angle=cellfun(@(x,y) x./y,wlist_w_vs_probe_dot,wlist_norms,'uni',false);
+%% 
+figure;
+subplot(1,2,1)
+imagesc(cell2mat(sent_w_vs_probe_angle'));
+colorbar()
+title('cosine angle between sentence and probe');
+subplot(1,2,2)
+imagesc(cell2mat(wlist_w_vs_probe_angle'));
+colorbar()
+title('cosine angle between wordlist and probe');
