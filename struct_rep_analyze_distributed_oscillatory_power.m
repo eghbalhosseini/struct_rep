11b195f4b1497ea2d7d3161ca05ab56ea9bb852e
kirsi0726 <66476247+kirsi0726@users.noreply.github.com>
2020-06-12 14:22:10 -0400

    Update
    Include jabber & non-word conditions, create new function calc_similarities for calculating angles (lines 105-112: need to display jabber & non-word results but returns error for these plots)

--- struct_rep_analyze_distributed_oscillatory_power.m
+++ struct_rep_analyze_distributed_oscillatory_power.m
@@ -2,19 +2,23 @@ clear all
 close all
 home 
 %% specify where the data is
-%data_path='C:\Users\kirsi\Documents\Git\UROP\struct_rep\data'; %change depending on user
-data_path='~/MyData/struct_rep/crunched/';
-%analysis_path=strcat(data_path,'analysis\distributed_oscilatory_power\');
-analysis_path=strcat(data_path,'analysis/distributed_oscilatory_power/');
+data_path='C:\Users\kirsi\Documents\Git\UROP\struct_rep\data'; %change depending on user
+%data_path='~/MyData/struct_rep/crunched/';
+analysis_path=strcat(data_path,'analysis\distributed_oscilatory_power\');
+%analysis_path=strcat(data_path,'analysis/distributed_oscilatory_power/');
 subject_id='AMC026';
-d_data= dir(strcat(data_path,'/',subject_id,'*_crunched_v2.mat'));
+d_data= dir(strcat(data_path,'\',subject_id,'*_crunched_v2.mat')); %change '/' depending on user
 fprintf(' %d .mat files were found \n', length(d_data));
-d_data=arrayfun(@(x) strcat(d_data(x).folder,'/',d_data(x).name),[1:length(d_data)]','uni',false);
+d_data=arrayfun(@(x) strcat(d_data(x).folder,'\',d_data(x).name),[1:length(d_data)]','uni',false); %change '/'
 %% combine responses from all sessions for a given condition. (S= sentence,....)
 sent_word_hilb_ave_tensor_all=[];
-wlist_word_hilb_ave_tensor_all=[];
 sent_probe_hilb_ave_tensor_all=[];
+wlist_word_hilb_ave_tensor_all=[];
 wlist_probe_hilb_ave_tensor_all=[];
+jab_word_hilb_ave_tensor_all=[];
+jab_probe_hilb_ave_tensor_all=[];
+non_word_hilb_ave_tensor_all=[];
+non_probe_hilb_ave_tensor_all=[];
 for k=1:length(d_data)
     subj=load(d_data{k});
     subj_id=fieldnames(subj);
@@ -22,7 +26,6 @@ for k=1:length(d_data)
     data=subj.data;
     info=subj.info;
     % sentences 
-
     cond='S';
     stim='word';
     data_out=extract_condition_response(data,info,cond,stim);
@@ -46,28 +49,64 @@ for k=1:length(d_data)
     data_out=extract_condition_response(data,info,cond,stim);
     wlist_probe_hilb_ave_tensor_all=cat(3,wlist_probe_hilb_ave_tensor_all,data_out);
     fprintf('added %s\n', strcat(d_data{k},'wlist probe'))
+    %jabber
+    cond='J';
+    stim='word';
+    data_out=extract_condition_response(data,info,cond,stim);
+    jab_word_hilb_ave_tensor_all=cat(3,jab_word_hilb_ave_tensor_all,data_out);
+    fprintf('added %s\n', strcat(d_data{k},'jabber word'))
+    %jabber, probe condition
+    cond='J'
+    stim='probe'
+    data_out=extract_condition_response(data,info,cond,stim);
+    jab_probe_hilb_ave_tensor_all=cat(3,jab_probe_hilb_ave_tensor_all,data_out);
+    fprintf('added %s\n', strcat(d_data{k},'jabber probe'))
+    %nonword
+    cond='N';
+    stim='word';
+    data_out=extract_condition_response(data,info,cond,stim);
+    non_word_hilb_ave_tensor_all=cat(3,non_word_hilb_ave_tensor_all,data_out);
+    %nonword, probe condition
+    cond='N';
+    stim='probe';
+    data_out=extract_condition_response(data,info,cond,stim);
+    non_probe_hilb_ave_tensor_all=cat(3,non_probe_hilb_ave_tensor_all,data_out);
 end 
 %% compute a cosine distance between word representation over all electrodes during sentence and during probe
 % sentence condition 
-sent_probe_norm=arrayfun(@(x)  sqrt(transpose(sent_probe_hilb_ave_tensor_all(:,:,x))*sent_probe_hilb_ave_tensor_all(:,:,x)),[1:size(sent_probe_hilb_ave_tensor_all,3)],'uni',false);
-sent_words_norm=arrayfun(@(x)  diag(sqrt(transpose(sent_word_hilb_ave_tensor_all(:,:,x))*sent_word_hilb_ave_tensor_all(:,:,x))),[1:size(sent_probe_hilb_ave_tensor_all,3)],'uni',false);
-sent_norms=cellfun(@(x,y) transpose(x.*y) ,sent_probe_norm,sent_words_norm,'uni',false);
-sent_w_vs_probe_dot=arrayfun(@(x)  transpose(sent_probe_hilb_ave_tensor_all(:,:,x))*sent_word_hilb_ave_tensor_all(:,:,x),[1:size(sent_word_hilb_ave_tensor_all,3)],'uni',false);
-sent_w_vs_probe_angle=cellfun(@(x,y) x./y,sent_w_vs_probe_dot,sent_norms,'uni',false);
+word_tensor=sent_word_hilb_ave_tensor_all;
+probe_tensor=sent_probe_hilb_ave_tensor_all;
+sentence_angle=calc_similarities(word_tensor,probe_tensor);
 
 % wordlist condition 
-wlist_probe_norm=arrayfun(@(x)  sqrt(transpose(wlist_probe_hilb_ave_tensor_all(:,:,x))*wlist_probe_hilb_ave_tensor_all(:,:,x)),[1:size(wlist_probe_hilb_ave_tensor_all,3)],'uni',false);
-wlist_words_norm=arrayfun(@(x)  diag(sqrt(transpose(wlist_word_hilb_ave_tensor_all(:,:,x))*wlist_word_hilb_ave_tensor_all(:,:,x))),[1:size(wlist_probe_hilb_ave_tensor_all,3)],'uni',false);
-wlist_norms=cellfun(@(x,y) transpose(x.*y) ,wlist_probe_norm,wlist_words_norm,'uni',false);
-wlist_w_vs_probe_dot=arrayfun(@(x)  transpose(wlist_probe_hilb_ave_tensor_all(:,:,x))*wlist_word_hilb_ave_tensor_all(:,:,x),[1:size(wlist_word_hilb_ave_tensor_all,3)],'uni',false);
-wlist_w_vs_probe_angle=cellfun(@(x,y) x./y,wlist_w_vs_probe_dot,wlist_norms,'uni',false);
-%% 
+word_tensor=wlist_word_hilb_ave_tensor_all;
+probe_tensor=wlist_probe_hilb_ave_tensor_all;
+wlist_angle=calc_similarities(word_tensor,probe_tensor);
+
+% jabber condition
+word_tensor=jab_word_hilb_ave_tensor_all;
+probe_tensor=jab_probe_hilb_ave_tensor_all;
+jab_angle=calc_similarities(word_tensor,probe_tensor);
+
+% nonword condition
+word_tensor=non_word_hilb_ave_tensor_all;
+probe_tensor=non_probe_hilb_ave_tensor_all;
+non_angle=calc_similarities(word_tensor,probe_tensor);
+
 figure;
 subplot(1,2,1)
-imagesc(cell2mat(sent_w_vs_probe_angle'));
+imagesc(cell2mat(sentence_angle'));
 colorbar()
 title('cosine angle between sentence and probe');
 subplot(1,2,2)
-imagesc(cell2mat(wlist_w_vs_probe_angle'));
+imagesc(cell2mat(wlist_angle'));
 colorbar()
 title('cosine angle between wordlist and probe');
+% subplot(1,2,3)
+% imagesc(cell2mat(jab_angle'));
+% colorbar()
+% title('cosine angle between jabber and probe');
+% subplot(1,2,4)
+% imagesc(cell2mat(non_angle'));
+% colorbar()
+% title('cosine angle between nonwords and probe');
