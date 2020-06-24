71e084eb94050b00edbb40abb67d88abef2b9bc8
kirsi0726 <66476247+kirsi0726@users.noreply.github.com>
2020-06-23 23:10:53 -0400

    Combines freqs for analysis

--- extract_condition_response.m
+++ extract_condition_response.m
@@ -5,9 +5,8 @@ function data_out=extract_condition_response(data,info,cond,stim)
     stim_loc=cellfun(@(x) find(strcmp(x.stimuli_type,stim)), data_cond,'uni',false);
     %old files -> x.signal_ave_hilbert_zs_downsample_parsed
     %new files -> x.signal_gaus_band_hilb_dec_zs_parsed
-    hilb_zs_ave_cell=cellfun(@(x) x.signal_gaus_band_hilb_dec_zs_parsed, data_cond,'uni',false);
-    %old files ->(stim_loc{x})
-    %new files -> (stim_loc{x},:)
+    %combine freqs -> x.combined_signal_gaus_zs_parsed
+    hilb_zs_ave_cell=cellfun(@(x) x.combined_signal_gaus_zs_parsed, data_cond,'uni',false);
     stim_zs_ave_cell=arrayfun(@(x) hilb_zs_ave_cell{x}(stim_loc{x},:),[1:size(hilb_zs_ave_cell,1)],'uni',false );
     % reshape stim to correct form if it is not
     stim_zs_ave_cell=cellfun(@(x) cell2mat(reshape(x,1,[])),stim_zs_ave_cell,'uni',false );
